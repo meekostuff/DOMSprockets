4fdfcbcc880103589f0da9e584d9c2cd13338406
Sean Hogan <shogun70@gmail.com>
2015-09-16 15:22:46 +1000

    Import Meeko.controllers from HyperFrameset

--- Sprocket.js
+++ Sprocket.js
@@ -1012,6 +1012,51 @@ return {
 
 })();
 
+Meeko.controllers = (function() { // TODO should this be under Meeko.sprockets??
+
+return {
+
+values: {},
+
+listeners: {},
+
+create: function(name) {
+        this.values[name] = [];
+        this.listeners[name] = [];
+},
+
+has: function(name) {
+        return (name in this.values);
+},
+
+get: function(name) { 
+        if (!this.has(name)) throw name + ' is not a registered controller';
+        return this.values[name];
+},
+
+set: function(name, value) {
+        if (!this.has(name)) throw name + ' is not a registered controller';
+        if (value === false || value == null) value = [];
+        else if (typeof value === 'string' || !('length' in value)) value = [ value ];
+        var oldValue = this.values[name];
+        if (_.difference(value, oldValue).length <= 0) return;
+        this.values[name] = value;
+        _.forEach(this.listeners[name], function(listener) {
+                Task.asap(function() { listener(value); });
+        });     
+},
+
+listen: function(name, listener) {
+        if (!this.has(name)) throw name + ' is not a registered controller';
+        this.listeners[name].push(listener);
+        var value = this.values[name];
+        Task.asap(function() { listener(value) });
+}
+
+};
+
+})();
+
 
 this.Meeko.sprockets = (function() {
 /* FIXME
